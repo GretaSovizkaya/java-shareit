060d94268eeb01fa2bb65994dce92ce4cdcbcc1c
Gerda <desgerda14@gmail.com>
2024-12-19 11:14:12 +0300

    checked fetches

--- src/main/java/booking/controller/BookingController.java
+++ src/main/java/booking/controller/BookingController.java
@@ -5,5 +5,5 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/bookings")
-public class BookingController {
-}
+public class BookingController { // пока оставим пустымпше
+}git 
